b2331c6686d44f1acc7f36b990524cd2bac62023
Buddy Moore <buddy.moore@gmail.com>
2019-08-11 18:36:46 -0400

    Transmit in batches

--- teleportchest.py
+++ teleportchest.py
@@ -36,6 +36,28 @@ def tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, srcconn, dstconn):
     exit(-1)
 
   cmd='''/silent-command 
+      surface = game.get_surface(1)
+      fef=surface.find_entity
+      dst=fef('steel-chest', {''' + dstx + ''', ''' + dsty + '''})
+      local inv = dst.get_inventory(defines.inventory.chest)
+      inv.sort_and_merge()
+      slots = 0
+      for x=1,#inv do
+        if inv[x].valid_for_read then
+          slots = slots + 1
+        end
+      end
+      rcon.print(#inv-slots)
+  '''
+
+  if dstconn[0]:
+    dstclient = dstconn[0]
+  else:
+    dstclient = factorio_rcon.RCONClient(dsthost, int(dstport), dstpass, timeout=5) 
+    dstconn[0] = dstclient
+  availableslots = dstclient.send_command(cmd)
+
+  cmd='''/silent-command 
 
       function serialize_table(t)
         s='{' 
@@ -53,19 +75,25 @@ def tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, srcconn, dstconn):
       surface = game.get_surface(1)
       fef=surface.find_entity
       src=fef('steel-chest', {''' + srcx + ''', ''' + srcy + '''})
-      inv = src.get_inventory(defines.inventory.chest)
+      local inv = src.get_inventory(defines.inventory.chest)
       contents = {}
       if inv.is_empty() then 
         rcon.print('empty')
       else
         inv.sort_and_merge()
         types=0
-        for k,_ in pairs(inv.get_contents()) do types = types + 1 end
-        name=inv[math.random(types)].name
-        qty = inv.find_item_stack(name).count
-        contents = {name=name,count=qty}
-        ser = serialize_table(contents)
-        rcon.print(ser)
+
+        for i=1,''' + availableslots + ''' do
+          --for k,_ in pairs(inv.get_contents()) do types = types + 1 end
+          --name=inv[math.random(types)].name
+          if inv[i].valid_for_read then
+            name=inv[i].name
+            qty = inv.find_item_stack(name).count
+            contents = {name=name,count=qty}
+            ser = serialize_table(contents)
+            rcon.print(ser)
+          end
+        end
       end
 
   '''
@@ -75,10 +103,10 @@ def tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, srcconn, dstconn):
   else:
     srcclient = factorio_rcon.RCONClient(srchost, int(srcport), srcpass, timeout=5)
     srcconn[0] = srcclient
-  contents = srcclient.send_command(cmd)
-  summary = contents
+  rsp = srcclient.send_command(cmd) or ''
+  contents = '{' + ','.join(rsp.split('\n')) + '}'
 
-  if contents != 'empty':
+  if contents != '[empty]':
     cmd='''/silent-command 
         surface = game.get_surface(1)
         fef=surface.find_entity
@@ -86,11 +114,13 @@ def tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, srcconn, dstconn):
 
         contents = ''' + contents + '''
 
-        if dst.can_insert(contents) then
-          dst.insert(contents)
-          rcon.print('ok')
-        else
-          rcon.print('nofit')
+        for k,v in pairs(contents) do
+          if dst.can_insert(v) then
+            dst.insert(v)
+            rcon.print('{' .. k .. ',' .. "'ok'}")
+          else
+            rcon.print('{' .. k .. ',' .. "'nofit'}")
+          end
         end
     '''
 
@@ -99,20 +129,21 @@ def tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, srcconn, dstconn):
     else:
       dstclient = factorio_rcon.RCONClient(dsthost, int(dstport), dstpass, timeout=5) 
       dstconn[0] = dstclient
-    result = dstclient.send_command(cmd)
-    summary = summary + ' ' + result 
+    result = dstclient.send_command(cmd) or ''
+    transferstatus = '{' + ','.join(result.split('\n')) + '}'
 
-    if result == 'ok':
-      cmd='''/silent-command 
-        contents = ''' + contents + '''
-        fef=surface.find_entity
-        src=fef('steel-chest', {''' + srcx + ''', ''' + srcy + '''})
-        src.get_inventory(defines.inventory.chest).remove(contents)
-        rcon.print('deleted')
-      '''
-      res = srcclient.send_command(cmd)
-      summary = summary + ' ' + res
-      print(summary)
+    cmd='''/silent-command 
+      fef=surface.find_entity
+      src=fef('steel-chest', {''' + srcx + ''', ''' + srcy + '''})
+      contents = ''' + contents + '''
+      transferstatus = ''' + transferstatus + '''
+      for k,v in pairs(transferstatus) do
+        if v[2] == 'ok' then
+          src.get_inventory(defines.inventory.chest).remove(contents[k])
+        end
+      end
+    '''
+    res = srcclient.send_command(cmd)
 
 if __name__ == '__main__':
   srcsrvid = os.sys.argv[1]
@@ -121,4 +152,4 @@ if __name__ == '__main__':
   dstsrvid = os.sys.argv[4]
   dstx = os.sys.argv[5]
   dsty = os.sys.argv[6]
-  tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty)
+  tp(srcsrvid, srcx, srcy, dstsrvid, dstx, dsty, [None], [None])
